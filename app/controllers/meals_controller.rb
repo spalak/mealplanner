021865289078053c2b859c83350704992c1be08a
Syam Palakurthy <syampalakurthy@gmail.com>
2015-07-18 15:27:34 -0700

    First commit for mealplanner project.  Added CRUD actions for meals

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,40 @@
+class MealsController < ApplicationController
+	def index
+		@meals = Meal.all
+	end
+
+	def new
+		@meals = Meal.new
+	end
+
+	def edit
+		@meals = Meal.find(params[:id])
+	end
+
+	def create
+		@meal = Meal.create(meal_params)
+		redirect_to meals_path(@meal)
+	end
+
+	def update
+		@meal = Meal.find(params[:id])
+		@meal.update(meal_params)
+		redirect_to meals_path
+	end
+
+	def destroy
+		@meal = Meal.find(params[:id])
+		@meal.destroy
+		redirect_to meals_path
+	end
+
+
+	def show
+	end
+
+	private
+		def meal_params
+			params.require(:meal).permit(:name, :description, :servings)
+		end
+
+end
---
+++ app/models/meal.rb
@@ -0,0 +1,4 @@
+class Meal <ActiveRecord::Base
+	def index
+	end
+end
