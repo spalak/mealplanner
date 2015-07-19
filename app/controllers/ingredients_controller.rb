3d9b36b810ca29966c353e982355112cb4a98c43
Syam Palakurthy <syampalakurthy@gmail.com>
2015-07-18 19:05:33 -0700

    Trying this commit again, to get app to include ingredients controller and views

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,37 @@
+class IngredientsController < ApplicationController
+	def index
+		@ingredients = Ingredient.all
+	end
+
+	def new
+		@ingredients = Ingredient.new
+	end
+
+	def edit
+		@ingredients = Ingredient.find(params[:id])
+	end
+
+	def create
+		@ingredients = Ingredient.create(ingredient_params)
+		redirect_to ingredients_path
+	end
+
+	def update
+		@ingredient = Ingredient.find(params[:id])
+		@ingredient.update(ingredient_params)
+		redirect_to ingredients_path
+
+	end
+
+	def destroy
+		@ingredient = Ingredient.find(params[:id])
+		@ingredient.destroy
+		redirect_to ingredients_path
+	end
+
+	private
+
+	def ingredient_params
+		params.require(:ingredient).permit(:name)
+	end
+end
